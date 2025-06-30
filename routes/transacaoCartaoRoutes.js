04a8a3abc7772079a97a42f1537d68625de4b72e
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-06-30 02:13:59 -0300

    feat: selecionar cobrança cartão adicionado

--- routes/transacaoCartaoRoutes.js
+++ routes/transacaoCartaoRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 // Rota para criar cobrança com cartão
 router.post('/', transacaoCartaoController.criarCobranca);
 router.get('/cartao/:id_cartao', transacaoCartaoController.selecionarCartao);
+router.get('/:id', transacaoCartaoController.selecionarTransacaoCartao);
 
 export default router;
